5d4782af5a387ee4251e56db812709cfdc1f0be2
binkybear <binkybear@nethunter.com>
2016-11-05 19:07:21 -0500

    Fix import line
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -16,7 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import to org.cyanogenmod.internal.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 import java.io.File;
 
 public class DisplayColorCalibration {
