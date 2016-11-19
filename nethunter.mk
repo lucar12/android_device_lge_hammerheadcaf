c56b452d591223e1b0b6f4303b71ec4b948c50ae
binkybear <binkybear@nethunter.com>
2016-11-19 01:32:38 -0600

    Fix vendor path nethunter.mk
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- nethunter.mk
+++ nethunter.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/nethunter/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerheadcaf/device.mk)
